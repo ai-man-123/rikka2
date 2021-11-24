ced49d8887e4882598642fcb834a59ec872da4f5
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-11-24 19:53:07 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -3,7 +3,7 @@ let moment = require('moment-timezone')
 let handler = async (m, { conn, args, isPrems }) => {
   if (!args[0]) throw 'Uhm...url nya mana?'
   m.reply('Downloading....')
-  let { nowatermark, watermark, audio } = (await require('../lib/tiktok')(args[0])).result
+  let { nowm: nowatermark, video: watermark, audio, preview } = (await require('../lib/musicaldown')(args[0])).result
      try {
   let { diggCount, text, playCount, commentCount, shareCount, createTime, authorMeta } = (await getVideoMeta(args[0])).collector[0]
   let { name, nickName } = authorMeta
@@ -12,17 +12,17 @@ let handler = async (m, { conn, args, isPrems }) => {
 
 - Name : ${nickName}
 - Username : ${name}
-- Likes : ${diggCount} Likes
-- Viewers : ${playCount} Viewers
-- Comment : ${commentCount} comment
-- Share : ${shareCount}
-- Upload At : ${moment(createTime * 1000).tz('Asia/Jakarta').format('HH/MM/YYYY hh:mm:ss')}
+- Likes : ${h2k(diggCount)} 
+- Viewers : ${h2k(playCount)} 
+- Comment : ${h2k(commentCount)} 
+- Share : ${h2k(shareCount)} 
+- Upload At : ${moment(createTime * 1000).tz('Asia/Jakarta').format('DD/MM/YYYY HH:mm:ss')}
 - Caption : ${text}
 `.trim())
   
- conn.sendMessage(m.chat, await getBuffer(nowatermark), 'videoMessage', { quoted: m, caption })
+ conn.sendMessage(m.chat, await getBuffer(nowatermark), 'videoMessage', { quoted: m, caption, thumbnail: await getBUffer(preview) })
 } catch (e) {
-    m.reply('Uploading....')
+    m.reply('Server 1 error, trying server 2...')
     conn.sendMessage(m.chat, await getBuffer(nowatermark), 'videoMessage', { quoted: m, caption: 'Jika Ingin Hasilnya Menjadi Audio, Silahkan Reply Video Ini dengan perintah #tomp3' })
 }
  
@@ -32,6 +32,20 @@ handler.tags = ['downloader']
 handler.command = /^(tiktok(dl)?)$/i
 
 module.exports = handler
+
+
+function h2k(number) {
+    var SI_POSTFIXES = ["", " K", " M", " G", " T", " P", " E"]
+    var tier = Math.log10(Math.abs(number)) / 3 | 0
+    if(tier == 0) return number
+    var postfix = SI_POSTFIXES[tier]
+    var scale = Math.pow(10, tier * 3)
+    var scaled = number / scale
+    var formatted = scaled.toFixed(1) + ''
+    if (/\.0$/.test(formatted))
+      formatted = formatted.substr(0, formatted.length - 2)
+    return formatted + postfix
+}
 
 async function getBuffer(url) {
 lets = await require('node-fetch')(url)
