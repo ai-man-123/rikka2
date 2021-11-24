d2d35bceb027e27ad9494b557b5a94b70f5dcee0
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-11-24 19:58:30 +0700

    Update tes.js

--- plugins/tes.js
+++ plugins/tes.js
@@ -3,6 +3,7 @@ let handler = m => m
 handler.before = async function (m, { conn, isAdmin, isBotAdmin }) {
 if (m.key.fromMe) return 
 if (m.isBaileys) return 
+if (!m.isGroup) return
   if (m.mentionedJid.includes(conn.user.jid)) {
 conn.sendFile(m.chat, './src/tag.webp', 'apsi tag tag', '', m)
    } else if (m.text.toLowerCase() == 'p') {
