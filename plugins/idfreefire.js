0d8d83c0a0430fcc3be0a109543253b1659f4522
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-11-25 22:58:28 +0700

    Update idfreefire.js

--- plugins/idfreefire.js
+++ plugins/idfreefire.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { args }) => {
-  let res = await fetch(global.API('xteam', '/search/freefire', {id: args[0]}, 'APIKEY'))
+  let res = await fetch(global.API('caliph', '/api/epep/id', {id: args[0]}, 'apikey'))
   let json = await res.json()
-  if (json.result.name) conn.reply(m.chat, json.result.name, m)
+  if (json.result) conn.reply(m.chat, json.result, m)
     else throw json
 }
 handler.help = ['epep'].map(v => v + ' <id>')
