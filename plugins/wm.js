49f9cdfe6e578976d1a63811e04690af4822411a
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-11-27 09:52:13 +0700

    Update wm.js

--- plugins/wm.js
+++ plugins/wm.js
@@ -3,7 +3,7 @@ const { addExif } = require('../lib/sticker')
 let handler = async (m, { conn, text }) => {
     let [packname, ...author] = text.split('|')
     author = (author || []).join('|')
-    let mime = m.quoted.mimetype || ''
+    let mime = (m.quoted ? m.quoted.mimetype : '')
     if (!/webp/.test(mime)) throw 'Reply sticker!'
     let img = await m.quoted.download()
     stiker = await addExif(img, packname || '', author || '')
