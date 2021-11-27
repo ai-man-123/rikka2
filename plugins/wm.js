310adcccfc1ced07e331687e8820fea50d35caa3
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-11-27 09:50:36 +0700

    Update wm.js

--- plugins/wm.js
+++ plugins/wm.js
@@ -1,20 +1,15 @@
 const { MessageType } = require('@adiwajshing/baileys')
-const { sticker } = require('../lib/sticker')
+const { addExif } = require('../lib/sticker')
 let handler = async (m, { conn, text }) => {
-  let stiker = false
-  try {
     let [packname, ...author] = text.split('|')
     author = (author || []).join('|')
     let mime = m.quoted.mimetype || ''
     if (!/webp/.test(mime)) throw 'Reply sticker!'
     let img = await m.quoted.download()
-    stiker = await sticker(img, false, packname || '', author || '')
-  } finally {
+    stiker = await addExif(img, packname || '', author || '')
     if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
     })
-    else throw 'Conversion failed'
-  }
 }
 handler.help = ['wm <packname>|<author>']
 handler.tags = ['sticker']
